04a31d263b6a6f85ef0ae0df65c2fa8b12b74e74
Dmitry <yakorovka@gmail.com>
2022-08-28 02:50:03 +0300

    add clustering

--- preparator.py
+++ preparator.py
@@ -1,6 +1,9 @@
 from sklearn import preprocessing
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.mixture import BayesianGaussianMixture
+import pandas as pd
+import numpy as np
 
 from sklearn.preprocessing import LabelEncoder
 from category_encoders import CatBoostEncoder
@@ -9,8 +12,8 @@ from category_encoders import OrdinalEncoder
 
 
 class DataPreparator:
-    def __init__(self, data):
-        self.data = data
+    def __init__(self):
+        # self.data = data
         self.is_cluster = False
         self.standard_scaler = StandardScaler()
         self.min_max_scaler = MinMaxScaler()
@@ -18,31 +21,44 @@ class DataPreparator:
         self.label_encoder = LabelEncoder()
         self.cat_boost_encoder = CatBoostEncoder()
         self.target_encoder = TargetEncoder()
+        self.clustelizer = None
 
     def transform(self, df,
                   fill_missing_categorical_by="mode",
-                  fill_missing_numerical_by="mode"):
+                  fill_missing_numerical_by=np.mean,
+                  type_data='train'):
         """
         Transform the data to the model
         :param df: Dataframe to transform
         :param fill_missing_categorical_by: Should fill missing categorical values
         :param fill_missing_numerical_by: Should fill missing numerical values
         """
+        new_df = df.copy()
         # Fill categorical missing values
-        cat_cols = df.select_dtypes(include=['object']).columns.values
+        cat_cols = new_df.select_dtypes(include=['object']).columns.tolist()
         for col in cat_cols:
             if fill_missing_categorical_by == "NaN":
-                df[col] = df[col].fillna('NaN')
+                new_df[col] = new_df[col].fillna('NaN')
             else:
-                df[col] = df.groupby('subject_name')[col].transform(lambda x: x.fillna(x.mode()))
+                new_df[col] = new_df.groupby('subject_name')[col].transform(lambda x: x.fillna(x.mode()))
 
         # Fill numerical missing values
-        num_cols = df.select_dtypes(include=['float64', 'int64']).columns.values
+        num_cols = new_df.select_dtypes(include=['float64', 'int64']).columns.tolist()
+        # new_df[num_cols] = new_df[num_cols].fillna("NaN")
         for col in num_cols:
-            df[col] = df.groupby('subject_name')[col].transform(fill_missing_numerical_by)
-        if self.is_cluster:
-            df['cluster'] = self._clusterize_data_(df)
-        pass
+            new_df[col] = new_df[col].fillna(new_df.groupby('subject_name')[col].transform(np.mean))
+        for col in num_cols:
+            new_df[col] = new_df[col].fillna(new_df.groupby('district')[col].transform(np.mean))
+
+        # TODO: как то переписать type_data
+        # TODO: add month and powertranform
+        f_cols = [i for i in num_cols if i.startswith('f')]
+        if self.is_cluster and type_data == 'train':
+            self._fit_cluster(new_df[f_cols])
+            new_df['cluster'] = self._clusterize_data_(new_df[f_cols])
+        elif self.is_cluster and type_data == 'test':
+            new_df['cluster'] = self._clusterize_data_(new_df[f_cols])
+        return new_df
 
     def fit(self,
             X,
@@ -71,9 +87,14 @@ class DataPreparator:
                    }
         encoder[type_of_encoder].fit(X, y)
 
-        # TODO: add clustering
         if is_clustering:
-            pass
+            self.is_cluster = True
 
-    def _clusterize_data_(self, df):
-        pass
+    def _fit_cluster(self, df: pd.DataFrame):
+        BGM = BayesianGaussianMixture(n_components=7, covariance_type='full', max_iter=300, n_init=5)
+        BGM.fit(df)
+        self.clustelizer = BGM
+
+    def _clusterize_data_(self, df: pd.DataFrame):
+        cluster_predict = self.clustelizer.predict(df)
+        return cluster_predict
