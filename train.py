64d69041a8f580b411fb515be7d0440359addccb
Sergey V <41781097+0x0000dead@users.noreply.github.com>
2022-08-28 10:10:51 +0700

    add catugra features

--- train.py
+++ train.py
@@ -10,7 +10,7 @@ from preparator import *
 
 
 def fix_seed(seed_value):
-    os.environ['PYTHONHASHSEED']=str(seed_value)
+    os.environ['PYTHONHASHSEED'] = str(seed_value)
     random.seed(seed_value)
     np.random.seed(seed_value)
 
@@ -19,13 +19,34 @@ if __name__ == "__main__":
     seed_value = 7575
     fix_seed(seed_value)
 
+    preparator = DataPreparator()
+    train_df = pd.read_csv('data/train.csv', sep=';')
+    val_df = pd.read_csv('data/test.csv', sep=';')
 
-    train_df = pd.read_csv('data/train_catugra_v2.csv', sep=',')
-    val_df = pd.read_csv('data/test_catugra_v2.csv', sep=',', index_col=0)
+    preparator.fit(train_df, train_df.label, is_clustering=False)
+    train_df = preparator.transform(df=train_df,
+                                    add_region_statistical_data=True,
+                                    add_rt_tariff_data=False,
+                                    add_covid_data=False,
+                                    fill_missing_categorical_by='NaN',
+                                    fill_missing_numerical_by=np.min,
+                                    type_data='train')
+    print("Train preparation ended")
+    val_df = preparator.transform(df=val_df,
+                                  add_region_statistical_data=True,
+                                  add_rt_tariff_data=False,
+                                  add_covid_data=False,
+                                  fill_missing_categorical_by='NaN',
+                                  fill_missing_numerical_by=np.min,
+                                  type_data='test')
+    print("Test preparation ended")
+
+    # train_df = pd.read_csv('data/train_catugra_v2.csv', sep=',')
+    # val_df = pd.read_csv('data/test_catugra_v2.csv', sep=',', index_col=0)
 
     cat_cols = train_df.select_dtypes(include=['object']).columns.values
-    train_df[cat_cols] = train_df[cat_cols].fillna('NaN')
-    val_df[cat_cols] = val_df[cat_cols].fillna('NaN')
+    # train_df[cat_cols] = train_df[cat_cols].fillna('NaN')
+    # val_df[cat_cols] = val_df[cat_cols].fillna('NaN')
 
     models = []
     skf = StratifiedKFold(n_splits=10, shuffle=True, random_state=seed_value)
@@ -37,7 +58,8 @@ if __name__ == "__main__":
         y_ts = train_df.label[test_ids]
 
         ctb = CatBoostClassifier(iterations=10, verbose=200, task_type='GPU', random_seed=seed_value, eval_metric='AUC')
-        ctb.fit(x_tr, y_tr, cat_features=cat_cols, eval_set=(x_ts, y_ts), use_best_model=True, early_stopping_rounds=150)
+        ctb.fit(x_tr, y_tr, cat_features=cat_cols, eval_set=(x_ts, y_ts), use_best_model=True,
+                early_stopping_rounds=150)
         models.append(ctb)
 
     for i, model in enumerate(models):
@@ -49,5 +71,3 @@ if __name__ == "__main__":
         pickle.dump(model, open(f'./models/model{i}.pckl', 'wb'))
 
     # label_val_df = val_df[['label']]
-
-
